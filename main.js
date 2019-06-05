2370412ccd881344dbd7af3fd00142c1719cab98
Tomas Milgron Bilbao <tomasmilgronbilbao@MMBA-B199.local>
2019-06-05 19:21:50 -0300

    firs steps

---
+++ main.js
@@ -0,0 +1,24 @@
+let player1Name = null;
+let player2Name = null;
+
+function getNames() {
+player1Name = document.querySelector(".player1Input").value;
+player2Name = document.querySelector(".player2Input").value;
+}
+
+let round = '';
+
+// Cells for the tic tac toe
+let A_1 = null;
+let A_2 = null;
+let A_3 = null;
+let B_1 = null;
+let B_2 = null;
+let B_3 = null;
+let C_1 = null;
+let C_2 = null;
+let C_3 = null;
+
+function winnerChecker() {
+
+}
